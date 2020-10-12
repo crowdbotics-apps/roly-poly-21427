bc22901f7a0442bd12dc3091ac9e1f2942f95da0
Crowdbotics <team@crowdbotics.com>
2020-10-12 23:00:19 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/roly_poly_21427/MainActivity.java
@@ -0,0 +1,15 @@
+package com.roly_poly_21427;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "roly_poly_21427";
+    }
+}
